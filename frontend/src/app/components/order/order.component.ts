7965b3f0eace1257b12fb67662c0046c020e5e0e
SiraphopSS <boomth8@gmail.com>
2024-09-13 23:54:28 +0700

    B6401894 - Add Category Model, Add Order Page

--- frontend/src/app/components/order/order.component.ts
+++ frontend/src/app/components/order/order.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit{
 
+  // fix Filter และ Menu
+
+  filters = ['All', 'Drinks', 'Desserts', 'Others'];
+
+  menu: any = [
+    { "name": "Latte", "category": "Drinks" },
+    { "name": "Mocha", "category": "Drinks" },
+    { "name": "Cappuccino", "category": "Drinks" },
+    { "name": "Espresso", "category": "Drinks" },
+    { "name": "Vanilla Cake", "category": "Desserts" },
+    { "name": "Mocha Cake", "category": "Desserts" },
+    { "name": "Cocoa Cake", "category": "Desserts" },
+    { "name": "Other", "category": "Others" },
+  ]
+
+  currentFilter = 'all';
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  filterMenu(category: string) {
+    this.currentFilter = category.toLowerCase();
+  }
+
+  isItemVisible(category: string): boolean {
+    return this.currentFilter === 'all' || category.toLowerCase() === this.currentFilter;
+  }
 }
