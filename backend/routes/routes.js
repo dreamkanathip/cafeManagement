26d2d9580ca835d2d896a1373fea82a92203d41f
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2024-09-15 00:07:59 +0700

    add controller and model payment

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,20 +2,30 @@ const { Router } = require("express");
 const authController = require("../controllers/authController");
 const MenuController = require("../controllers/MenuController");
 const CategoryController = require("../controllers/CategoryController");
+const PaymentController = require("../controllers/PaymentController"); // Import PaymentController
 const authenticateToken = require("../middleware/authMiddleware");
 
 const router = Router();
-//login register logout
+
+// Login, register, logout
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/user", authenticateToken, authController.getUser);
 router.post("/logout", authController.logout);
+
 // Menu
 router.post("/addMenu", MenuController.addMenu);
 router.get("/allMenu", MenuController.getMenu);
+
 // Category
 router.post("/addCategory", CategoryController.addCategory);
 router.get("/categories", CategoryController.getAllCategory);
 router.get("/category/:category", CategoryController.getCategoryByName);
+
+// Payment
+router.post("/addPayment", PaymentController.addPayment);
+router.get("/payments", PaymentController.getAllPayments);
+router.patch("/payment", PaymentController.updatePayment);
+router.delete("/payment", PaymentController.deletePayment);
 
 module.exports = router;
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { IngerdientComponent } from './components/inventory/ingerdient/ingerdient.component';
 import { MenuComponent } from './components/inventory/menu/menu.component';
@@ -11,19 +10,17 @@ import { OrderComponent } from './components/order/order.component';
 import { EditMenuComponent } from './components/inventory/menu/edit/edit.component';
 import { AddMenuComponent } from './components/inventory/menu/add/add.component';
 
-
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'inventory', component: InventoryComponent},
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'inventory', component: InventoryComponent },
   { path: 'inventory/manage-menu', component: MenuComponent },
   { path: 'inventory/manage-menu/edit', component: EditMenuComponent },
   { path: 'inventory/manage-menu/add', component: AddMenuComponent },
-  { path: 'inventory/manage-ingredient', component: IngerdientComponent},
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
-  { path: 'order', component: OrderComponent},
-
+  { path: 'inventory/manage-ingredient', component: IngerdientComponent },
+  { path: 'order', component: OrderComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
