295f22005eeee700bbdd30f407e40b34a6331a93
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2024-09-22 19:56:50 +0700

    Your commit message

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const multer = require('multer'); // Import multer for file uploads
+const multer = require("multer"); // Import multer for file uploads
 const authController = require("../controllers/authController");
 const MenuController = require("../controllers/MenuController");
 const CategoryController = require("../controllers/CategoryController");
@@ -17,24 +17,27 @@ router.get("/user", authenticateToken, authController.getUser);
 router.post("/logout", authController.logout);
 
 // Menu
-router.post("/addMenu", upload.single('image'), MenuController.addMenu);
+router.post("/addMenu", upload.single("image"), MenuController.addMenu);
 router.get("/allMenu", MenuController.getMenu);
 router.get("/menu/:_id", MenuController.getMenuById);
-router.delete("/menu/:_id", MenuController.deleteMenu)
-router.put("/menuUpdate/:_id",upload.single('image'), MenuController.updateMenu)
+router.delete("/menu/:_id", MenuController.deleteMenu);
+router.put(
+  "/menuUpdate/:_id",
+  upload.single("image"),
+  MenuController.updateMenu
+);
 
 // Category
-router.get("/allCategory", CategoryController.getAllCategory)
-router.post("/addCategory", CategoryController.addCategory)
-router.get("/category/:_id", CategoryController.getCategoryById)
-router.delete("/category/:_id", CategoryController.deleteCategory)
-router.put("/categoryUpdate/:_id", CategoryController.updateCategory)
+router.get("/allCategory", CategoryController.getAllCategory);
+router.post("/addCategory", CategoryController.addCategory);
+router.get("/category/:_id", CategoryController.getCategoryById);
+router.delete("/category/:_id", CategoryController.deleteCategory);
+router.put("/categoryUpdate/:_id", CategoryController.updateCategory);
 
 // Payment
-router.post("/addPayment", PaymentController.addPayment);
-router.get("/payments", PaymentController.getAllPayments);
-router.patch("/payment", PaymentController.updatePayment);
-router.delete("/payment", PaymentController.deletePayment);
-
+// router.post("/addPayment", PaymentController.addPayment);
+// router.get("/payments", PaymentController.getAllPayments);
+// router.patch("/payment", PaymentController.updatePayment);
+// router.delete("/payment", PaymentController.deletePayment);
 
 module.exports = router;
