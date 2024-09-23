f9cee169eb521b413d345d1f701739dc87b7b429
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2024-09-23 22:33:57 +0700

    fix css order and inventory

--- frontend/src/app/components/inventory/inventory.component.ts
+++ frontend/src/app/components/inventory/inventory.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Emitters } from '../../emitters/emitter'; // Adjust the path as necessary
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.css']
+  styleUrls: ['./inventory.component.css'],
 })
-export class InventoryComponent {
-
-}
+export class InventoryComponent {}
